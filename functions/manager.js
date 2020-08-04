91bcbc4da976cc960257aa3b7215f3ebe57537ab
paquitosoft <paquitosoftware@gmail.com>
2020-08-04 21:58:29 +0200

    Added content-type header to response

--- functions/manager.js
+++ functions/manager.js
@@ -2,6 +2,9 @@ exports.handler = async event => {
 	const url = event.queryStringParameters.hlsUrl || '';
 	return {
 		statusCode: 200,
+		headers: {
+			'Content-Type': 'application/json'
+		},
 		body: JSON.stringify({ url })
 	};
 };
